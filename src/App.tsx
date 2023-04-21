a6fec45352c0c11da04bcd933cecc97e46ac8a71
Arthur De Souza Silva <arthurdesouza.silv@gmail.com>
2023-04-21 03:46:41 -0300

    Atualizacao de cores e imagens

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,8 @@ import { Box } from './components/Box'
 function App() {
 
   return (
-    <div className='p-10 bg-blue-200 h-screen flex flex-col justify-center items-center'>
+    <div className='p-10 bg-slate-900 h-screen flex flex-col justify-center items-center'>
       <Box/>
-
-      <a 
-        className='block shadow bg-green-600 text-white text-center p-4 mt-5 rounded-xl hover:bg-green-500'
-        href="">
-        Quero conhecer mais
-      </a>
     </div>
   )
 }
--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,16 +1,24 @@
 export const Box = () => {
     return (
-        <div className="bg-gray-100 p-8 shadow rounded-xl overflow-hidden md:flex md:p-0">
-            <img className="w-32 h-32 rounded-full mx-auto md:rounded-none md:w-60 md:h-auto " src="./public/aluno.jpg" alt="Aluno" />
-            <div className="pt-6 text-center space-y-4 md:text-left md:p-8">
-                <div className="text-lg font-semibold">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda, sit! Sint eos culpa sed asperiores esse impedit, nulla inventore expedita veniam reiciendis dignissimos perspiciatis atque blanditiis, ipsam, quam numquam dolore?
-                </div>
-                <div className="font-midium">
-                    <div className="text-blue-600 font-bold">Arthur de souza</div>
-                    <div className="text-gray-500">Aluno da blabblalabla</div>
+        <div>
+            <div className="bg-gray-100 p-8 shadow rounded-xl overflow-hidden md:flex md:p-0">
+                <img className="w-32 h-32 rounded-full mx-auto md:rounded-none md:w-60 md:h-auto " src="./public/Eu.jpeg" alt="Foto minha" />
+                <div className="pt-6 text-center space-y-4 md:text-left md:p-8">
+                    <div className="text-lg font-semibold">
+                        Desenvolvedor front-end, apaixonado por criar sites e aplicativos. Já domino HTML, CSS e JavaScript e estou sempre em busca de aprender mais.
+                    </div>
+                    <div className="font-midium">
+                        <div className="text-blue-600 font-bold">Arthur de souza silva</div>
+                        <div className="text-gray-500">Desenvolvedor Front-end</div>
+                    </div>
                 </div>
             </div>
+            <a
+                className='block shadow bg-blue-600 text-white text-center p-4 mt-5 rounded-xl hover:bg-blue-500'
+                href="https://www.linkedin.com/in/arthur-de-souza-47807723a/">
+                Conheça mais!!!
+            </a>
+
         </div>
     )
 }
